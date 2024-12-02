c2d9461316497ab766c8b4cc10e464f3dc876766
szymn1 <szymon.lange@gmail.com>
2024-12-02 17:00:52 +0100

    add part 2 of task 1

--- 1.py
+++ 1.py
@@ -1,6 +1,10 @@
+from operator import countOf
+
+
 l1 = []
 l2 = []
 dist = 0
+similarity_score = 0
 
 with open('input', 'r') as f:
     for line in f:
@@ -10,6 +14,13 @@ with open('input', 'r') as f:
 
 l1.sort()
 l2.sort()
+l1 = [int(i) for i in l1]
+l2 = [int(i) for i in l2]
 
-dist = sum([abs(int(i) - int(j)) for i, j in zip(l1, l2)])
-print(dist)
+
+dist = sum([abs(i - j) for i, j in zip(l1, l2)])
+print(f'Dist: {dist}')
+
+similarity_score = sum([i * countOf(l2, i) for i in l1])
+
+print(f'Similarity score: {similarity_score}')
